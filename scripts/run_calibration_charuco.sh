5c5f48d9af7f89b80067779ab1f3f35932706a23
Raghav Navaratna <rsnavaratna@gmail.com>
2023-10-04 19:42:17 -0500

    [ADD] new calibration data with charuco 14x10 board

--- scripts/run_calibration_charuco.sh
+++ scripts/run_calibration_charuco.sh
@@ -9,7 +9,7 @@ fi
 # calib_test_dir="../../st_handeye_eval/calib_test"
 
 camera_params="../calibration_data/20230920_camera_info.yaml"
-calib_test_dir="../calibration_data/20230920"
+calib_test_dir="../calibration_data/20231004"
 
 # reconst_dir="../../st_handeye_eval/spirit_reconst"
 
